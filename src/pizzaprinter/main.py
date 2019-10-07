51dea74c781764a38f8c357054d262a5f0021d4a
svalouch <svalouch@valouch.com>
2019-10-07 07:26:06 +0200

    main: enable printing to comply with docs, leave file output as example

--- src/pizzaprinter/main.py
+++ src/pizzaprinter/main.py
@@ -14,7 +14,8 @@ app = FastAPI()
 @app.post('/receipt', status_code=201)
 def receipt(receipt: Receipt):
     print(f'Received receipt data: {receipt}')
-    with open('/tmp/receipt.bin', 'w') as fh:
-        fh.write(render('templates/receipt.j2', receipt))
-    # with serial.Serial('/dev/ttyUSB0', 19200, timeout=10) as ser:
-    #     ser.write(render('templates/receipt.j2', receipt))
+    # uncomment the following two lines to dump the result to a file for inspection:
+    # with open('/tmp/receipt.bin', 'w') as fh:
+    #     fh.write(render('templates/receipt.j2', receipt))
+    with serial.Serial('/dev/ttyUSB0', 19200, timeout=10) as ser:
+        ser.write(render('templates/receipt.j2', receipt))
