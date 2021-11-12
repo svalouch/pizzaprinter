03e8c441ada70a2bea275bf09d992678c5b32f8c
svalouch <svalouch@valouch.com>
2021-11-12 14:14:56 +0100

    replace some characters for codepage 858

--- src/pizzaprinter/main.py
+++ src/pizzaprinter/main.py
@@ -18,4 +18,16 @@ def receipt(receipt: Receipt):
     # with open('/tmp/receipt.bin', 'w') as fh:
     #     fh.write(render('templates/receipt.j2', receipt))
     with serial.Serial('/dev/ttyUSB0', 19200, timeout=10) as ser:
-        ser.write(render('templates/receipt.j2', receipt))
+        data = render('templates/receipt.j2', receipt).encode('utf-8')
+        repl = {
+            'ä': bytes([0x84]),
+            'Ä': bytes([0x8e]),
+            'ö': bytes([0x94]),
+            'Ö': bytes([0x99]),
+            'ü': bytes([0x81]),
+            'Ü': bytes([0x9a]),
+            'ß': bytes([0xe1]),
+        }
+        for c_src, c_dst in repl.items():
+            data = data.replace(c_src.encode('utf-8'), c_dst)
+        ser.write(data)
